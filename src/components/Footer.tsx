26680b6c06749bfae3fe7b2d80e8bd25745bacdf
Lorenzo Cestaro <cestaro.lorenzo@gmail.com>
2021-12-05 19:17:02 +0100

    feat: add links to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,22 +3,42 @@ import React from "react";
 import clsx from "clsx";
 
 const styles = {
+  break: clsx("hidden sm:block"),
   footer: clsx(
     "flex",
     "flex-col",
+    "sm:flex-row",
     "space-y-1",
+    "sm:space-y-0",
+    "space-x-1",
     "justify-center",
     "items-center",
+    "align-baseline",
     "w-full",
     "h-16",
     "mt-8",
+    "pb-4",
+    "sm:pb-0",
     "text-sm",
     "text-gray-400",
   ),
+  link: clsx("border-dotted", "border-b-2", "hover:border-solid", "cursor:pointer"),
 };
 
 const Footer = () => (
-  <footer className={styles.footer}>&copy; {new Date().getFullYear()} Lorenzo Cestaro</footer>
+  <footer className={styles.footer}>
+    <p>&copy; {new Date().getFullYear()} Lorenzo Cestaro</p>
+    <span className={styles.break}> · </span>
+    <span>
+      <a className={styles.link} href="https://github.com/lorenzocestaro/lorenzocestaro.github.io">
+        Source code
+      </a>
+      <span> · </span>
+      <a className={styles.link} href="https://www.google.com/maps/search/bars+near+me/">
+        Buy yourself a beer
+      </a>
+    </span>
+  </footer>
 );
 
 export default Footer;
