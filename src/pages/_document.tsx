ccec71adc4e3d2555b6ad43cd00dc06398a31449
Lorenzo Cestaro <cestaro.lorenzo@gmail.com>
2021-12-06 20:30:08 +0100

    feat: add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import clsx from "clsx";
-
 import Document, { DocumentContext, Html, Head, Main, NextScript } from "next/document";
 
 const styles = {
@@ -34,6 +33,8 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Mulish:wght@200;300;400;500;600;700;800;900&family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
             rel="stylesheet"
           />
+          <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
         </Head>
         <body className={styles.body}>
           <Main />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,10 +19,6 @@ const Home: React.FC = () => (
   <div className={styles.container}>
     <Head>
       <title>Home · Lorenzo Cestaro </title>
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="manifest" href="/site.webmanifest" />
     </Head>
 
     <main className={styles.main}>
