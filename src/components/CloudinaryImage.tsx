6eacd12a58804d5f5da6176f72015bb747768a7b
Lorenzo Cestaro <cestaro.lorenzo@gmail.com>
2024-01-15 23:40:37 +0100

    photo page

--- src/components/CloudinaryImage.tsx
+++ src/components/CloudinaryImage.tsx
@@ -1,7 +1,6 @@
 import { AdvancedImage, lazyload, placeholder } from "@cloudinary/react";
 import { dpr } from "@cloudinary/url-gen/actions/delivery";
 import { scale } from "@cloudinary/url-gen/actions/resize";
-import Link from "next/link";
 import { type RenderPhotoProps } from "react-photo-album";
 
 import { config } from "src/config";
@@ -9,7 +8,7 @@ import { config } from "src/config";
 export const CloudinaryImage = ({
   photo,
   wrapperStyle,
-  imageProps: { alt, title, sizes, className, onClick },
+  imageProps: { alt, title, sizes, className },
 }: RenderPhotoProps) => {
   const image = config.cloudinary
     .image(photo.src)
@@ -19,23 +18,17 @@ export const CloudinaryImage = ({
 
   return (
     <div style={{ ...wrapperStyle, position: "relative" }}>
-      <Link
-        className="cursor-zoom-in"
-        href={`/photo/${encodeURIComponent(photo.src)}`}
-      >
-        <AdvancedImage
-          alt={alt}
-          className={className}
-          cldImg={image}
-          onClick={onClick}
-          plugins={[
-            lazyload({ rootMargin: "100px" }),
-            placeholder({ mode: "blur" }),
-          ]}
-          sizes={sizes}
-          title={title}
-        />
-      </Link>
+      <AdvancedImage
+        alt={alt}
+        className={className}
+        cldImg={image}
+        plugins={[
+          lazyload({ rootMargin: "100px" }),
+          placeholder({ mode: "blur" }),
+        ]}
+        sizes={sizes}
+        title={title}
+      />
     </div>
   );
 };
