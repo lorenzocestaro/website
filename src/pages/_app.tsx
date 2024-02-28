a9fb93d68dd03e8ea64274f14a7335f91ff0ac98
Lorenzo Cestaro <cestaro.lorenzo@gmail.com>
2024-02-28 23:12:50 +0100

    add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
-import "regenerator-runtime/runtime"; // Needed for imagekitio-reac
 import React from "react";
 
+import { Analytics } from "@vercel/analytics/react";
 import { IKContext } from "imagekitio-react";
 import { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 
+import "regenerator-runtime/runtime"; // Needed for imagekitio-react
 import "tailwindcss/tailwind.css";
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
@@ -12,6 +13,7 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
     <ThemeProvider enableSystem={false} defaultTheme="light" attribute="class">
       <IKContext urlEndpoint={process.env.NEXT_PUBLIC_IMAGEKIT_URL}>
         <Component {...pageProps} />
+        <Analytics />
       </IKContext>
     </ThemeProvider>
   );
